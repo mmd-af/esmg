906133a745e2d9b564dac7053685e9024532d1a2
thepermonica <99553574+thepermonica@users.noreply.github.com>
2022-12-26 11:41:50 +0330

    introducing-services is ok

--- resources/views/site/sections/introducing-services.blade.php
+++ resources/views/site/sections/introducing-services.blade.php
@@ -1,5 +1,6 @@
 <div class="container-fluid shadow-lg my-5 py-4 h-100 border-top bg-primary">
     <div class="row">
+        <h1>معرفی خدمات</h1>
         <div class="col-sm-12 col-lg-4">
             <div class="card rounded-3 card-span shadow">
                 <img class="img-fluid" src="https://dl.musicalacademy.ir/musicalacademy/storage/photos/videocourses/guitarLesonMohammadAfshar.jpg" alt="">
@@ -9,7 +10,7 @@
             <div class="row">
                 <div class="col-sm-12 col-md-6">
                     <div class="card rounded-3 card-span shadow p-4 h-100 border-top border-4 border-primary">
-                        <h1>box1</h1>
+                        <h2>box1</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores consequatur,
                             debitis dolorum ducimus explicabo illum inventore ipsam iste modi mollitia, numquam, porro
                             quaerat quas quasi quis rem totam vel? </p>
@@ -17,7 +18,7 @@
                 </div>
                 <div class="col-sm-12 col-md-6">
                     <div class="card rounded-3 card-span shadow p-4 h-100 border-top border-4 border-primary">
-                        <h1>box1</h1>
+                        <h2>box2</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores consequatur,
                             debitis dolorum ducimus explicabo illum inventore ipsam iste modi mollitia, numquam, porro
                             quaerat quas quasi quis rem totam vel? </p>
@@ -25,7 +26,7 @@
                 </div>
                 <div class="col-sm-12 col-md-6">
                     <div class="card rounded-3 card-span shadow p-4 h-100 border-top border-4 border-primary">
-                        <h1>box1</h1>
+                        <h2>box3</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores consequatur,
                             debitis dolorum ducimus explicabo illum inventore ipsam iste modi mollitia, numquam, porro
                             quaerat quas quasi quis rem totam vel? </p>
@@ -33,7 +34,7 @@
                 </div>
                 <div class="col-sm-12 col-md-6">
                     <div class="card rounded-3 card-span shadow p-4 h-100 border-top border-4 border-primary">
-                        <h1>box1</h1>
+                        <h2>box4</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores consequatur,
                             debitis dolorum ducimus explicabo illum inventore ipsam iste modi mollitia, numquam, porro
                             quaerat quas quasi quis rem totam vel? </p>
