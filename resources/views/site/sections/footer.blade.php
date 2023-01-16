631a20a42d8a8dedc9961662a2bcd90d5d9f5b65
thepermonica <99553574+thepermonica@users.noreply.github.com>
2023-01-16 15:43:06 +0330

    map Ok

--- resources/views/site/sections/footer.blade.php
+++ resources/views/site/sections/footer.blade.php
@@ -1,25 +1,43 @@
 <div class="container-fluid bg-gradiant-1 reveal-content">
     <div class="row">
-        <div class="col-6 col-md-4 col-xl-4 mb-3">
+        <div class="col-sm-12 col-md-4 col-lg-3 mb-3">
+            <div class="text-white p-4">
+                <i class="fas fa-address-book fa-3x"></i>
+            </div>
+            <ul class="list-unstyled mb-md-4 mb-lg-0 text-white content-zoom">
+                <li class="lh-lg"><i class="fas fa-envelope text-white"></i> info@esmg.co.ir</li>
+                <li class="lh-lg"><i class="fas fa-phone-square text-white"></i> 09376925054</li>
+                <li class="lh-lg"><i class="fas fa-phone-square text-white"></i> 071-36240650</li>
+            </ul>
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-3 mb-3 text-white">
+            <div class="p-4">
+                <i class="fas fa-hashtag fa-3x"></i>
+            </div>
+            <ul class="list-unstyled mb-md-4 mb-lg-0 content-zoom">
+                <li class="lh-lg"><i class="fab fa-instagram"></i> @esmg</li>
+                <li class="lh-lg"><i class="fab fa-whatsapp"></i> 09376925054</li>
+                <li class="lh-lg"><i class="fas fa-phone"></i> 071-36240650</li>
+            </ul>
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-3 mb-3">
             <div class="text-white p-4">
                 <i class="fas fa-map-marker-alt fa-3x"></i>
             </div>
-
             <ul class="list-unstyled mb-md-4 mb-lg-0 content-zoom text-white">
                 <li class="lh-lg"><p>شیراز - شهرک آرین - خ فناوری - پارک علم و فناوری فارس - ساختمان تخت جمشید -
                         واحد 3010</p>
                 </li>
             </ul>
         </div>
-        <div class="col-6 col-md-4 col-xl-4 mb-3">
+        <div class="col-sm-12 col-md-4 col-lg-3 mb-3">
             <div class="text-white p-4">
-                <i class="fas fa-address-book fa-3x"></i>
+                <i class="fas fa-map fa-3x"></i>
             </div>
-            <ul class="list-unstyled mb-md-4 mb-lg-0 content-zoom">
-                <li class="lh-lg"><i class="fas fa-envelope text-white"></i> info@esmg.co.ir</li>
-                <li class="lh-lg"><i class="fas fa-phone-square text-white"></i> 09376925054</li>
-                <li class="lh-lg"><i class="fas fa-phone-square text-white"></i> 071-36240650</li>
-            </ul>
+            <iframe class="img-thumbnail"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3464.830797233837!2d52.459315015720776!3d29.724660140561728!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3fb21a115caa0311%3A0xc9f48e432ab44733!2zRmFycyBQcm92aW5jZSwgU2hpcmF6LCBEaXN0cmljdCAxMNiMINm-2KfYsdqpINi52YTZhSDZiCDZgdmG2KfZiNix24wg2YHYp9ix2LMsIElyYW4!5e0!3m2!1sen!2suk!4v1673871003556!5m2!1sen!2suk"
+                    allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
     <hr/>
