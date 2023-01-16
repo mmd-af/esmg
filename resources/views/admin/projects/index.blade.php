c2edd69f426e86ba199f933b016282afd8f4c6e8
thepermonica <99553574+thepermonica@users.noreply.github.com>
2023-01-16 09:10:13 +0330

    Add Upload file to gitignore and fix project index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -44,7 +44,7 @@
                                 {{ $project->project_location }}
                             </td>
                             <td>
-                                <img src="{{ url('/upload/projects/' . $project->logo_image) }}"
+                                <img src="{{$project->logo_image}}"
                                      width="50px" height="50px" alt="{{ $project->project_name }}">
                             </td>
                             <td>
@@ -84,7 +84,7 @@
     </div>
     @foreach ($projects as $project)
         <div class="modal fade" id="productModal-{{$project->id}}" tabindex="-1" role="dialog">
-            <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-dialog modal-xl" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -95,13 +95,17 @@
                         <div class="row">
                             <div class="col-md-6 col-sm-6 col-xs-6" style="direction: rtl;">
                                 <div class="project-details-content quickview-content">
-                                    <h2 class="text-right mb-4">
+                                    <div class="text-right mb-4">
                                         <img class="card-img-small"
-                                             src="{{ url('/upload/projects/' . $project->logo_image) }}" alt=""/>
-                                        {{ $project->project_name }}</h2>
-                                    <p class="text-right">
-                                        {{ $project->description }}
-                                    </p>
+                                             src="{{ $project->logo_image }}" alt=""/>
+                                        <h2>{{ $project->project_name }}</h2>
+                                    </div>
+                                    <div class="text-right m-5">
+                                        <div class="alert alert-warning small">
+                                            در اینجا محتوای پروژه به صورت ساده شده نمایش میدهد.
+                                        </div>
+                                        {!! strip_tags($project->description) !!}
+                                    </div>
                                     <div class="pro-details-meta">
                                         <span>نام کارفرما:</span>
                                         <ul>
@@ -120,52 +124,16 @@
                                             <li>{{ $project->year_enforce }}</li>
                                         </ul>
                                     </div>
-                                    <div class="pro-details-meta">
-                                        <ul>
-                                            <span>رضایت مشتری</span>
-                                            <li>
-                                                <a href="{{ url('/upload/projects/' . $project->customer_image) }}"
-                                                   data-lightbox="image-1"><img class="card-img-small"
-                                                                                src="{{ url('/upload/projects/' . $project->customer_image) }}"
-                                                                                alt=""/></a>
-                                            </li>
-                                        </ul>
-                                    </div>
                                 </div>
                             </div>
                             <div class="col-md-5 col-sm-12 col-xs-12">
                                 <div class="tab-content quickview-big-img">
                                     <div id="pro-primary-{{$project->id}}" class="tab-pane fade show active">
-                                        <img src="{{ url('/upload/projects/' . $project->primary_image) }}"
+                                        <img src="{{ $project->primary_image }}"
                                              width="300px" alt=""/>
-                                    </div>
-                                    @foreach ($project->images as $image)
-                                        <div id="pro-{{$image->id}}" class="tab-pane fade">
-                                            <img width="300px" src="{{ url('/upload/projects/' . $image->image) }}"
-                                                 alt=""/>
-                                        </div>
-                                    @endforeach
-                                </div>
-                                <!-- Thumbnail Large Image End -->
-                                <!-- Thumbnail Image End -->
-                                <div class="quickview-wrap mt-15">
-                                    <div class="quickview-slide-active owl-carousel nav nav-style-2" role="tablist">
-                                        <a class="active" data-toggle="tab" href="#pro-primary-{{$project->id}}">
-                                            <img class="card-img-small"
-                                                 src="{{ url('/upload/projects/' . $project->primary_image) }}"
-                                                 width="200px" alt=""/>
-                                        </a>
-                                        @foreach ($project->images as $image)
-                                            <a data-toggle="tab" href="#pro-{{$image->id}}">
-                                                <img class="card-img-small"
-                                                     src="{{ url('/upload/projects/' . $image->image) }}" alt=""/>
-                                            </a>
-                                        @endforeach
                                     </div>
                                 </div>
                             </div>
-
-
                         </div>
                     </div>
 
